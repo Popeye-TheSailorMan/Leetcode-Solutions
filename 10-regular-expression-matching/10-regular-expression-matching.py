36f1e90bcdae9eae8ea5a2a3722d0d02a28c5da5
Abhishek Saxena <44522820+Popeye-TheSailorMan@users.noreply.github.com>
2022-06-12 00:54:55 +0530

    Time: 119 ms (29.53%), Space: 13.9 MB (89.81%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.py
+++ 10-regular-expression-matching/10-regular-expression-matching.py
@@ -1,8 +1,5 @@
 import re
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        # if ('*'in p or '.' in p):
-        #     x = re.search(p,s)
-        #     return x
         pattern = ('^'+p+'$')
         return re.match(pattern,s)
