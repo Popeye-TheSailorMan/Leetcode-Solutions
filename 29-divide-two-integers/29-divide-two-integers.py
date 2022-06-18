44a9353f9f29f2b8922f44b50043ed724f70727c
Abhishek Saxena <44522820+Popeye-TheSailorMan@users.noreply.github.com>
2022-06-18 23:03:42 +0530

    Time: 30 ms (97.45%), Space: 13.8 MB (75.48%) - LeetHub

---
+++ 29-divide-two-integers/29-divide-two-integers.py
@@ -0,0 +1,9 @@
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        if (dividend==-2147483648 and divisor==-1):
+            return 2147483647
+        else:
+            a = (dividend/divisor)
+            s = str(a).split('.')
+            return(int(s[0]))
+            
